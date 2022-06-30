9b9ec91f5614c9781cb308f0f31a246524ae63ae
saikat2990 <bsse0910@iit.du.ac.bd>
2022-06-30 21:44:28 +0000

    PredictPartyVictory add

---
+++ Topics/STack/Stack/Stack/PredictPartyVictory.cs
@@ -0,0 +1,58 @@
+﻿public class Solution
+{
+    public string GenerateAnswer(string senate)
+    {
+        Dictionary<string, string> victory = new Dictionary<string, string>();
+        victory.Add("R", "Radiant");
+        victory.Add("D", "Dire");
+        string result = "";
+        int RCount = 0;
+        int DCount = 0;
+
+        var data = new Queue();
+        foreach (var key in senate)
+        {
+            data.Enqueue(key.ToString());
+            if (key.ToString() == "R") RCount++;
+            else DCount++;
+        }
+
+        while (true)
+        {
+            var victoryKey = data.GetHeadValue();
+            Console.WriteLine(victoryKey + " " + DCount.ToString() + " " + RCount.ToString());
+            if (victoryKey == "R")
+            {
+                DCount -= 1;
+                data.Remove("D");
+                if (DCount == 0) return "Radiant";
+            }
+
+            else
+            {
+                RCount -= 1;
+                data.Remove("R");
+                if (RCount == 0) return "Diet";
+            }
+            data.Dequeue();
+            data.Enqueue(victoryKey);
+            data.show();
+        }
+    }
+
+    public string PredictPartyVictory(string senate)
+    {
+        return GenerateAnswer(senate);
+    }
+}
+
+class Program1
+{
+    public static void Main1(string[] args)
+    {
+        var solution = new Solution();
+        var answer = solution.PredictPartyVictory(
+            "DDR");
+        Console.WriteLine(answer);
+    }
+}
