823198efc4b8beef144a920ce87c98758109391d
saikat2990 <bsse0910@iit.du.ac.bd>
2022-06-30 10:22:56 +0000

    341. Flatten Nested List Iterator

---
+++ Topics/STack/Stack/Stack/341. Flatten Nested List Iterator.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public interface NestedInteger
+{
+    bool IsInteger();
+    int GetInteger();
+    IList<NestedInteger> GetList();
+}
+
+public class NestedIterator
+{
+    private IList<int> result { get; set; }
+    private int index { get; set; }
+
+    public NestedIterator(IList<NestedInteger> nestedList)
+    {
+        result = new List<int>();
+        index = 0;
+        AddValue(nestedList);
+    }
+
+    public void AddValue(IList<NestedInteger> nestedList)
+    {
+        foreach (var list in nestedList)
+        {
+            if (list.IsInteger())
+            {
+                result.Add(list.GetInteger());
+            }
+            else
+            {
+                AddValue(list.GetList());
+            }
+        }
+    }
+
+    public bool HasNext()
+    {
+        return index < result.Count;
+    }
+
+    public int Next()
+    {
+        return result[index++];
+    }
+}
